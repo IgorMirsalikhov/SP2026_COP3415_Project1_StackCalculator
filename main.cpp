90c739added96831ec4bb97e66decd465b2a8b2a
Igor Mirsalikhov <imirsalikhov@floridapoly.edu>
2026-02-19 21:21:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <stdexcept>
+#include <iomanip>
 
 int main(void) {
     std::string infixExpression;
@@ -13,11 +14,14 @@ int main(void) {
     try {
         std::string postfix = calc.toPostfix();
         std::cout << "Postfix expression: " << postfix << std::endl;
+        std::cout << std::fixed << std::setprecision(3);
         std::cout << "Result: " << calc.calculate() << std::endl;
     }
     catch (std::exception& ex) {
         std::cout << ex.what() << std::endl;
     }
 
+
     return 0;
+
 }
